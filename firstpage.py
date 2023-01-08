b11fcbe58fcd389f7a395e492e23407975f80486
AbhroChowdhury <abhrajyo@ualberta.ca>
2023-01-07 19:53:31 -0700

    tweaks

--- firstpage.py
+++ firstpage.py
@@ -9,7 +9,7 @@ root.title("Stock App")
 
 LoginFrame = CTkFrame(root, width = 800, height = 100, fg_color='#1F1F1F')
 LoginFrame.place(x=300, y=0, anchor=tkinter.N)
-label = ctk.CTkLabel(master=root, text="InvestIStock", bg_color='#1F1F1F', font=('SF Pro',40))
-label.place(x=175, y=20)
+label = ctk.CTkLabel(master=root, text="InvestIStock", bg_color='#1F1F1F', font=('Arial',40))
+label.place(x=185, y=20)
 
 root.mainloop()
