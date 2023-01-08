f5bdce370f28523c75993c43fb9c9ab0b5b9880c
ryanalmasi <108089503+ryanalmasi@users.noreply.github.com>
2023-01-08 01:08:10 -0700

    Changes

--- functions.py
+++ functions.py
@@ -28,10 +28,11 @@ def balancePull(ticker, period, years_of_data):
             'longTermDebt', 'totalCurrentLiabilities', 'otherNonCurrentLiabilities', 'totalNonCurrentLiabilities', 'totalLiabilities',
             'totalStockholdersEquity', 'totalLiabilitiesAndStockholdersEquity'] #
     balance_dict = {}
-    proper_keys = ['Cash and Cash Equivalents', 'Short Term Investments', 'Net Receivables', 'Inventory', 'Other Current Assets', 'Total Current Assets',
-    'Property Plant Equipment Net', 'Other Non-Current Assets', 'Total Non-Current Assets', 'Total Assets', 'Account Payables', 'Other Current Liabilities', 'Deferred Revenue',
-    'Long Term Debt', 'Total Current Liabilities', 'Other Non-Current Liabilites', 'Total Non-Current Liabilites', 'Total Liabilites', "Total Stockholders' Equity",
-    "Total Liabilities and Stockholders' Equity"]
+    proper_keys = ['Cash and Cash Equivalents', 'Short Term Investments', 'Net Receivables', 'Inventory', 
+            'Other Current Assets', 'Total Current Assets', 'Property Plant Equipment Net', 'Other Non-Current Assets', 
+            'Total Non-Current Assets', 'Total Assets', 'Account Payables', 'Other Current Liabilities', 'Deferred Revenue',
+            'Long Term Debt', 'Total Current Liabilities', 'Other Non-Current Liabilites', 'Total Non-Current Liabilites', 
+            'Total Liabilites', "Total Stockholders' Equity", "Total Liabilities and Stockholders' Equity"]
 
     for entry in results:
         for key in entry:
@@ -53,15 +54,37 @@ def cashflowPull(ticker, period, years_of_data):
             'netChangeInCash', 'cashAtBeginningOfPeriod', 'cashAtEndOfPeriod', 'capitalExpenditure', 'operatingCashFlow', 
             'freeCashFlow']
     cashflow_dict = {}
-    proper_keys = []
+    proper_keys = ['Net Income', 'Depreciation and Amortization', 'Stock-Based Compensation', 'Deferred Income Tax', 
+            'Change in Working Capital', 'Accounts Receivables', 'Inventory', 'Other Working Capital', 'Accounts Payable', 
+            'Other Non Cash Items', 'Net Cash Provided by Operating Activities', 'Investments in Property Plant and Equipment', 
+            'Acquisitions Net', 'Purchases of Investments', 'OtherInvestingActivities', 'Net Cash Used for Investing Activities', 
+            'Debt Repayment','Common Stock Repurchased', 'Dividends Paid', 'Net Cash Used Provided by Financing Activities', 
+            'Net Change in Cash', 'Cash at Beginning of Period','Cash At End of Period', 'Capital Expenditure', 
+            'Operating Cash Flow', 'Free Cash Flow']
 
     for entry in results:
         for key in entry:
             if key in cashflow_keys:
                 cashflow_dict[key] = results[0][key]
     cashflow_dict = {proper_keys[cashflow_keys.index(key)]: value for key, value in cashflow_dict.items()}
+    return cashflow_dict
 
-print(cashflowPull(ticker, "annual", 1))
+def incomePull(ticker, period, years_of_data):
+    url = f"https://financialmodelingprep.com/api/v3/income-statement/{ticker}?Limit=[{years_of_data}&Period={period}&apikey=662675887f17041bd4ed6406d2fb2ff8"
+    results = requests.get(url).json()
+    income_keys = ['revenue', 'costOfRevenue', 'grossProfit', 'researchAndDevelopmentExpenses', 'operatingExpenses', 'costAndExpenses', 
+            'interestIncome', 'interestExpense', 'operatingIncome', 'incomeBeforeTax', 'incomeTaxExpense', 'netIncome',
+            'eps', 'epsdiluted']
+    income_dict = {}
+    proper_keys = []
+    for entry in results:
+        for key in entry:
+            if key in income_keys:
+                income_dict[key] = results[0][key]
+    income_dict = {proper_keys[income_keys.index(key)]: value for key, value in income_dict.items()}
+    return income_dict
+
+print(incomePull(ticker, period, 1))
 
 def growthPull(type, ticker):
     url = f"https://financialmodelingprep.com/api/v3/{type}-statement-growth/{ticker}?apikey=662675887f17041bd4ed6406d2fb2ff8"
